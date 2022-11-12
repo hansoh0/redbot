3989b85cdd3a0256a7696257c3e239df9f020caa
canyon <48212912+saucecan@users.noreply.github.com>
2022-11-11 21:14:23 -0500

    Update redbotPUSH.py

--- redbotPUSH.py
+++ redbotPUSH.py
@@ -1,37 +1,34 @@
 #! /usr/bin/env python3
 
 # FILE: redbotPUSH.py
-# PURPOSE: Reddit commie reply bot
+# PURPOSE: Reddit reply bot
 
-import praw
-import pdb
-import re
-import os
-import random
+import praw, pdb, re, os, random
 
-quotelist = ["The revolution will be glorious, comrade.","Believe in yourself comrade! Our time will come.", "The revolution is coming, comrade."]
+quotelist = ["","", ""]
 # reddit instance login and password
 reddit = praw.Reddit(client_id='CLIENT_ID',
 client_secret='CLIENT_SECRET',
 password='PASSWORD',
 username='redthecomrade')
 
+def replyManager():
+	#creating a list for repeats
+	if not os.path.isfile("posts_replied_to.txt"):
+		posts_replied_to = []
 
-#creating a list for repeats
-if not os.path.isfile("posts_replied_to.txt"):
-	posts_replied_to = []
+	#or load list of posts replied to
+	else:
+		with open("posts_replied_to.txt","r") as f:
+			posts_replied_to = f.read()
+			posts_replied_to = posts_replied_to.split("\n")
+			posts_replied_to = list(filter(None, posts_replied_to))
 
-#or load list of posts replied to
-else:
-	with open("posts_replied_to.txt","r") as f:
-		posts_replied_to = f.read()
-		posts_replied_to = posts_replied_to.split("\n")
-		posts_replied_to = list(filter(None, posts_replied_to))
-
-#top 10 newest entries
-subreddit = reddit.subreddit('SUBREDDIT')
-for submission in subreddit.new(limit=100):
-	#looks through replied to 
+def getReddit():
+	#top 10 newest entries
+	subreddit = reddit.subreddit('SUBREDDIT')
+	for submission in subreddit.new(limit=100):
+		#looks through replied to 
 	if submission.id not in posts_replied_to:
 		#ignores case
 		if re.search("our", submission.title, re.IGNORECASE): 
@@ -42,6 +39,9 @@ for submission in subreddit.new(limit=100):
 			#store id
 			posts_replied_to.append(submission.id)
 
-with open("posts_replied_to.txt", "w") as f:
-	for post_id in posts_replied_to:
-		f.write(post_id + "\n")
+def __main__():
+	with open("posts_replied_to.txt", "w") as f:
+		for post_id in posts_replied_to:
+			f.write(post_id + "\n")
+if __name__=="__main__"():
+	__main__()
